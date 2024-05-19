1ed856226e51af9b8f789b8f3d574a878d1c4b12
AungChoLatt <aungcholatt888@gmail.com>
2024-05-19 13:33:29 +0630

    first commit complete

---
+++ Swift_Messenger_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift_Messenger_App
+//
+//  Created by macair on 19/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift_Messenger_App/Swift_Messenger_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Messenger_AppApp.swift
+//  Swift_Messenger_App
+//
+//  Created by macair on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Messenger_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
