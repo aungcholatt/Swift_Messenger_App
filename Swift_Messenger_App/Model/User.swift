50028f0733bde9b6d91e1915b90332aee102813b
AungChoLatt <aungcholatt888@gmail.com>
2024-06-04 12:47:48 +0630

    Messenger page Completed

--- Swift_Messenger_App/Model/User.swift
+++ Swift_Messenger_App/Model/User.swift
@@ -17,13 +17,13 @@ struct User: Codable, Identifiable, Hashable{
     var id: String {
         return uid ?? NSUUID().uuidString
     }
-}
 
     var firstName: String {
         let formatter = PersonNameComponentsFormatter()
         let components = formatter.personNameComponents(from: fullname)
-        return components?givenName ?? fullname
+        return components?.givenName ?? fullname
     }
+ }
 
 extension User{
     static let MOCK_USER = User(fullname: "Thein Naing", email: "thein@gmail.com", profileImageUrl: "thein")
